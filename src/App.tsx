a2671a4cfc5c32b8577f00d4c05680baaa8ab5e0
onwuzulike emeka <texyh@outlook.com>
2019-09-16 21:42:35 +0100

    setup ingredients

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import Layout from './components/Layout/Layout'
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
 export default class App extends Component {
   render() {
     return (
       <div>
         <Layout>
-          <p>Burger</p>
+          <BurgerBuilder />
         </Layout>
       </div>
     );
--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,10 +1,10 @@
 import React, {FC, Fragment} from 'react'
-
+import classes from './Layout.module.css';
 const layout:FC = (props) => {
     return (
         <Fragment>
         <div>ToolBar, SideBar, BackDrop</div>
-        <main>
+        <main className={classes.Content}>
             {props.children}
         </main>
         </Fragment>
